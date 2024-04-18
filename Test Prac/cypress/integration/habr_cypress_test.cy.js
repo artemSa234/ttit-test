7921fb40c8468f9433c2ad5f9d4aaba0535a0ba3
Artem <156803343+artemSa234@users.noreply.github.com>
2024-04-18 20:53:04 +0700

    Add files via upload

---
+++ Test Prac/cypress/integration/habr_cypress_test.cy.js
@@ -0,0 +1,52 @@
+describe('Cypress Tests', () => {
+    it('None-existent login test', () => { 
+        cy.fixture('cypressTests').then(data => {
+            cy.log('Переход на страницу авторизации')
+            cy.visit(data.main_url)
+
+            cy.log('Ввод несуществующего логина')
+            cy.get('input[id="passp-field-login"]')
+                .type(data.none_existent_login)
+
+            cy.log('Клик по кнопке "Войти"')
+            cy.get('div[class="passp-button passp-sign-in-button"] button[type="submit"]')
+                .click()
+
+            cy.log('Проверка что появился элемент сигнализирующий об ошибке')
+            cy.get('div[class="passp-form-field_error"]')
+                .should('exist')
+        
+        })
+    })
+
+    it('None-existent password test', () => { 
+        cy.fixture('cypress Tests').then(data => { 
+        cy.log('Переход на страницу авторизации') 
+        cy.visit(data.main_url)
+
+        cy.log('Ввод логина')
+        cy.get('input[id="passp-field-login"]')
+            .type(data.login)
+
+        cy.log('Клик по кнопке "Войти"')
+        cy.get('div[class="passp-button passp-sign-in-button"] button [type="submit"]')
+            .click()
+        // ожидаем 2 секунды для появления формы ввода пароля
+        cy.wait(2000);
+
+        cy.log('Ввод несуществующего пароля') 
+        cy.get('input[id="passp-field-passwd"]') 
+            .type(data.none_existent_password)
+
+        cy.log( 'Клик по кнопке "Войти"')
+        cy.get('div[class="passp-button passp-sign-in-button"] button [type="submit"]')
+            .click()
+
+        cy.log('Проверка что появился элемент сигнализирующий об ошибке') 
+        cy.get('div[class="passp-form-field_error"]')
+        .should('exist')
+
+        });
+    });
+
+})
